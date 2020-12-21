28deffadd134c2025cb6babddf853212dbabe877
Anvar HunT <anvar.hunt@gmail.com>
2020-12-22 02:24:20 +0600

    update sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,10 +34,9 @@
 
         <footer class="main-footer">
             <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.0.3-pre
+                <b>Версия</b> 0.0.1
             </div>
-            <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
-            reserved.
+            <strong>Копирайт гы-гы-гы &copy; 2020-2021</strong> Все права защищены
         </footer>
         <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
